e23fe26dbffd6ab9afcbd46b76d834f1d035ddb8
anbilli <anbilli@umich.edu>
2017-07-03 14:03:44 -0400

    Working for lunch calculation

--- background.js
+++ background.js
@@ -1,25 +1,8 @@
-// Called when the user clicks on the browser action
-// chrome.browserAction.onClicked.addListener(function(tab) {
-	// Send a message to the active tab
-	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		// var activeTab = tabs[0];
-		// chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-	// });
-// });
-
 // Retrieves message from content.js
 chrome.runtime.onMessage.addListener(function (request, sender) {
   // First, validate the message's structure
   if ((request.from === 'content') && (request.subject === 'showPageAction')) {
     // Enable the page-action for the requesting tab
     chrome.pageAction.show(sender.tab.id);
-	console.log("###############################################here in background");
   }
 });
-
-// chrome.runtime.onMessage.addListener(
-	// function(request, sender, sendResponse) {
-		// document.getElementById("weirdest_cat_ever").innerHTML = request.endTime;
-		// console.log("in background");
-	// }
-// );
