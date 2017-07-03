4b67df33ddfea840856cc3db30509ff0799d4dbb
anbilli <anbilli@umich.edu>
2017-07-03 14:03:22 -0400

    Working for lunch calculation

--- content.js
+++ content.js
@@ -5,66 +5,46 @@ chrome.runtime.sendMessage({
   subject: 'showPageAction'
 });
 
-
 // Listen for messages from popup
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if( request.from === 'popup' && request.subject === 'DOMInfo' ) {
-			// window.alert("here");
 			
 			// Grab page elements
-			// var tbody = document.getElementById("divActivities").children[0].children[0];
-			// var trow1 = tbody.children[0].innerHTML;
-			// var trow2 = tbody.children[1].innerHTML;
-			// var trow3 = tbody.children[2].innerHTML;
+			var tbody = document.getElementById("divActivities").children[0].children[0];
+			var scans = tbody.children.length;
 			
-			// Extract times
-			// var startTime = trow1.substring(trow1.indexOf(':')-2, trow1.indexOf(':')+3);
-			// var lunchStart = trow2.substring(trow2.indexOf(':')-2, trow2.indexOf(':')+3);
-			// var lunchEnd = trow3.substring(trow3.indexOf(':')-2, trow3.indexOf(':')+3);
+			// Parse times
+			var startTime = null;
+			var lunchStart = null;
+			var lunchEnd = null;
 			
-			// Convert times to minutes
-			// startTime = convertToMins(startTime);
-			// lunchStart = convertToMins(lunchStart);
-			// lunchEnd = convertToMins(lunchEnd);
-			
-			// Calculate end time for 8 hour day
-			// var endTime = startTime + (8 * 60) + (lunchEnd - lunchStart);
+			if (scans > 0) {
+				var trow1 = tbody.children[0].innerHTML;
+				startTime = parseTime(trow1);	
+			}
+			if (scans > 2) {
+				var trow2 = tbody.children[1].innerHTML;
+				var trow3 = tbody.children[2].innerHTML;
 
-			// Convert back to hours
-			// endTime = convertToHours(endTime);
-			
-			// Convert result to JSON
-			var val = document.getElementsByTagName("title")[0].innerHTML;
-			var bool = false;
+				lunchStart = parseTime(trow2);
+				lunchEnd = parseTime(trow3);
+			}
+		
+			// Convert results to JSON
 			var results = {
-				// endTime: endTime
-				endTime: val,
-				luncher: bool
+				startTime: startTime,
+				lunchStart: lunchStart,
+				lunchEnd: lunchEnd
 			};
 			
 			// Return results to extension	
 			sendResponse(results);
-		    // chrome.runtime.sendMessage({"endTime": endTime});
-			// window.alert("To achieve an 8 hour work day, you need to clock-out at " + endTime);
-
 		}
 });
 
-
-// Input: "hh:mm", string
-// Output: minutes, int
-function convertToMins(time) {
-	var hours = parseInt(time.slice(0, time.indexOf(":")));
-	var minutes = parseInt(time.slice(time.indexOf(":") + 1));
-	return (60 * hours + minutes);
+// Parses time from row content
+function parseTime(input) {
+	return input.substring(input.indexOf(':')-2, input.indexOf(':')+3);
 }
 
-// Input: minutes, int
-// Output: "hh:mm", string
-function convertToHours(time) {
-	var hour = (Math.floor(time / 60)).toString();
-	var min = (time % 60).toString();
-	min = ("0" + min).slice(-2);
-	return (hour + ":" + min.toString());
-}
